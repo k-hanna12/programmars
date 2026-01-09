47d601b0014db345e040534ffa82de640b6f3954
k-hanna12 <108797616+k-hanna12@users.noreply.github.com>
2026-01-09 21:01:46 +0900

    Add phone number validation solutions
    Implemented two solutions for phone number validation: one using sorting and the other using a hash set for prefix checking.

---
+++ 해시/전화번호목록.py
@@ -0,0 +1,24 @@
+#정렬기반 풀이 / 문자열 정렬하여 이웃한 문자(=접두어 유사가능성 존재)만 확인
+def solution(phone_book):
+    answer = True
+
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        a = phone_book[i]
+        b = phone_book[i+1][:len(a)]
+        if (b == a): 
+            return False
+    return True
+  
+#해시기반 풀이 /앞에서부터 쪼개서 해당 문자가 hash 안에 있는지 확인
+def solution(phone_book):
+
+    phone_hash = set(phone_book)
+
+    for i in phone_book:
+        temp = ""
+        for j in i[:-1]:
+            temp += j
+            if temp in phone_hash:
+                return False
+    return True
